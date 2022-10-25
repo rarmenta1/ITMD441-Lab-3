d5fa6351923fda39de4e08fc01041552b9241aea
Rafael Armenta <rarmenta@hawk.iit.edu>
2022-10-25 00:07:39 -0500

    adjusted bill total
    the bill total will now constantly update the calculations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+
+function updateBill(val) {
+
+  // Tip Percent slider
+  var bill = parseFloat(document.getElementById('bill_total_field').value = val);
+
+  var billPercent = parseFloat(document.getElementById('bill_percent_field').value);
+
+  var tipPercent = parseFloat(document.getElementById('tip_percent_field').value =  `${billPercent}%`);
+  
+  //calculate bill amount
+  var tipValue = tipPercent * (val/100);
+  tipValue = tipValue.toFixed(2);
+ 
+
+
+  document.getElementById('tip_amount_field').value= `${'$' + tipValue}`;
+
+  val = +val + +tipValue;
+
+
+  document.getElementById('total_bill_field').value=`${'$' + val}`;
+
+
+}
+
+
 function updateTipInput(val) {
 
-  
   // Tip Percent slider
   var tipPercent = document.getElementById('tip_percent_field').value = `${val}%`;
 
