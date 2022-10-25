a891550131385012010cf812d8d82c4eda4bd33a
Rafael Armenta <rarmenta@hawk.iit.edu>
2022-10-24 22:58:33 -0500

    Update script.js
    wrote javascript calculations for the Tip Calculator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 function updateTipInput(val) {
-  document.getElementById('tip_percent_field').value=val;
-  val = val + 2;
-  document.getElementById('tip_amount_field').value=val
+
+  
+  // Tip Percent slider
+  var tipPercent = document.getElementById('tip_percent_field').value = `${val}%`;
+
+  //assign bill_total_field to the bill
+  var bill = Number(document.getElementById('bill_total_field').value);
+
+  //calculate bill amount
+  var tipValue = val * (bill/100);
+  tipValue = tipValue.toFixed(2);
+  var grandTotal = bill + (val * (bill/100));
+  grandTotal = grandTotal.toFixed(2);
+
+
+
+  document.getElementById('tip_amount_field').value=`${'$' + tipValue}`;
+
+  document.getElementById('total_bill_field').value=`${'$' + grandTotal}`;
+
+
 }
